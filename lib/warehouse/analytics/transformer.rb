f139e290c7277ede883e0a80abaad41d41ffd539
Ethan <emmoistner@gmail.com>
2022-05-12 12:10:51 -0400

    Transformer Event, Key, Data Type Validation  (#6)
    * [feature] snake_case event value and all message keys
    
    * [cleanup] normalize to rename_properties and fix transform method execution
    
    * [cleanup] simplify snake_case
    
    * [feature] Add data type validation to transformer flatten_hash
    
    * [feature] Stringify all property keys
    
    * [refactor] optimize reserved word prefixing via hashmap

--- lib/warehouse/analytics/transformer.rb
+++ lib/warehouse/analytics/transformer.rb
@@ -1,3 +1,5 @@
+require 'warehouse/analytics/logging'
+
 module Warehouse
   class Analytics
     # Handles transforming fields according to the Warehouse Spec
@@ -14,38 +16,63 @@ module Warehouse
     #
     class Transformer
       class << self
+        include Warehouse::Analytics::Logging
+
+        VALID_VALUE_TYPES = [String, Numeric, TrueClass, FalseClass, Time, DateTime, Array, Hash]
 
         def transform(message)
-          flattened_message = flatten(message)
-          prefix_reserved_words(flattened_message)
+          transformed_message = { **message }
+          transformed_message = flatten_and_transform_hash(transformed_message, true)
+          transformed_message = rename_properties(transformed_message)
+          prefix_reserved_words(transformed_message)
         end
 
-        def flatten(message)
-          flatten_hash(message, true)
+        def rename_properties(message)
+          message["event_text"] = message["event"] if message["event"].present?
+          message["event"] = snake_case(message["event"]) if message["event"].present?
+          message["id"] = message["messageId"] if message["messageId"].present?
+          message
         end
 
         def prefix_reserved_words(message)
-          message[:event] = prefix_redshift_reserved_words(message[:event]) if message[:event].present?
+          message["event"] = prefix_redshift_reserved_words(message["event"]) if message["event"].present?
           message.transform_keys {|key| prefix_redshift_reserved_words(key) }
         end
 
         private
 
         def prefix_redshift_reserved_words(word)
-          Defaults::Redshift::RESERVED_WORDS.find { |e| /#{word}/i =~ e }.present? ? "_#{word}" : word
+          Defaults::Redshift::RESERVED_WORDS[word.upcase] ? "_#{word}" : word
         end
 
-        def flatten_hash(hash, top_level)
+        def snake_case(word)
+          # Based on underscore from rails active support
+          # https://github.com/wycats/rails-api/blob/4aa40d1381fac5bc69bae6bb8e24dfb421997b40/vendor/rails/activesupport/lib/active_support/inflector/methods.rb#L38
+          word.to_s
+            .gsub(/::|:|\//, '_')                   # Convert all ::, :, / to _
+            .gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2')   # Insert _ between capital letter preceding lower case letter (ignores the start of the word)
+            .gsub(/([a-z])([A-Z])/,'\1_\2')         # Insert _ between lower case letter preceding a capital letter
+            .tr("- ", "_")                          # Convert hyphens - and spaces to underscore
+            .downcase                               # Convert all capital letters to lower case
+        end
+
+        def valid_value_type?(value)
+          VALID_VALUE_TYPES.find { |valid_class| value.is_a? valid_class }
+        end
+
+        def flatten_and_transform_hash(hash, top_level)
           hash.each_with_object({}) do |(k, v), h|
             if v.is_a? Hash
               key_prefix = ((top_level && k == :context) || !top_level) ? "#{k}_" : ""
-              flatten_hash(v, false).map do |h_k, h_v|
-                h["#{key_prefix}#{h_k.downcase}".to_sym] = h_v
+              flatten_and_transform_hash(v, false).map do |h_k, h_v|
+                h["#{key_prefix}#{snake_case(h_k)}".to_s] = h_v
               end
             elsif v.is_a? Array
-              h[k] = "[#{v.join(',')}]"
+              h[snake_case(k)] = "[#{v.join(',')}]"
+            elsif valid_value_type?(v)
+              h[snake_case(k)] = v
             else
-              h[k] = v
+              logger.warn "Unexpected Data Type (#{v.class}) in flatten_and_transform_hash for key (#{k})"
             end
            end
         end
--- spec/warehouse/analytics/transformer_spec.rb
+++ spec/warehouse/analytics/transformer_spec.rb
@@ -12,21 +12,21 @@ module Warehouse
           message = { properties: { nested_array: ['Gang', 'is', 'all', 'here'] } }
           expect(Transformer.transform(message)).to eq({
             nested_array: '[Gang,is,all,here]'
-          })
+          }.stringify_keys)
         end
 
         it 'stringifies all context fields that contain a nested array' do
           message = { context: { roles: ['admin', 'manager'] } }
           expect(Transformer.transform(message)).to eq({
             context_roles: '[admin,manager]'
-          })
+          }.stringify_keys)
         end
 
         it 'stringifies all traits that contain a nested array' do
           message = { traits: { address: { street: '1129 E 16th St, Indianapolis, IN 46202'} } }
           expect(Transformer.transform(message)).to eq({
             address_street: '1129 E 16th St, Indianapolis, IN 46202'
-          })
+          }.stringify_keys)
         end
 
         it '“flattens” all properties that contain a nested object' do
@@ -51,8 +51,9 @@ module Warehouse
             nested_object_deeply_nested_object_deepest_nested_object_deepiest_nested_object_yes: "deepiest",
             nested_object_2_mild_depth: true,
             shallow: "shallow"
-          })
+          }.stringify_keys)
         end
+
         it '“flattens” all traits that contain a nested object' do
           message = {
             traits: {
@@ -75,7 +76,7 @@ module Warehouse
             nested_object_deeply_nested_object_deepest_nested_object_deepiest_nested_object_yes: "deepiest",
             nested_object_2_mild_depth: true,
             shallow: "shallow"
-          })
+          }.stringify_keys)
         end
 
         it '“flattens” all context fields that contain a nested object' do
@@ -105,29 +106,80 @@ module Warehouse
             context_another_object_field_1: 'this is field 1',
             context_another_object_field_2: 'this is field 2',
             context_another_object_field_3: 'this is field 3'
-          })
+          }.stringify_keys)
+        end
+
+        it 'snake_cases event name with : and /' do
+          message = {
+            event: "Test:Event/GreatName"
+          }
+          expect(Transformer.transform(message)).to eq({
+            event_text: "Test:Event/GreatName",
+            event: "test_event_great_name"
+          }.stringify_keys)
+        end
+
+        it 'snake_cases event name with :: namespace and spaces' do
+          message = {
+            event: "Test::Event PascalCase"
+          }
+          expect(Transformer.transform(message)).to eq({
+            event_text: "Test::Event PascalCase",
+            event: "test_event_pascal_case"
+          }.stringify_keys)
+        end
+
+        it 'snake_cases all keys' do
+          message = {
+            properties: {
+              nestedObject: {
+                NotSnakeCase: "Please"
+              }
+            },
+            context: {
+              NotSnakeCase: "At all"
+            },
+            traits: {
+              camelCase: "oops"
+            }
+          }
+          expect(Transformer.transform(message)).to eq({
+            nested_object_not_snake_case: "Please",
+            context_not_snake_case: "At all",
+            camel_case: "oops"
+          }.stringify_keys)
+        end
+
+        it 'ignores unexpected value data type' do
+          message = {
+            properties: {
+              unknown_type: RuntimeError.new("We don't handle error types!")
+            }
+          }
+          expect(Transformer.transform(message)).to eq({})
         end
 
         it 'prefixes reserved event and keys' do
           message = {
-            event: Defaults::Redshift::RESERVED_WORDS[2],
-            properties: Defaults::Redshift::RESERVED_WORDS.to_h { |k| [(rand 2) == 0 ? k.downcase : k.upcase, k] }
+            event: "ALL",
+            properties: Defaults::Redshift::RESERVED_WORDS.to_h { |k| [(rand 2) == 0 ? k.downcase : k.upcase, true] }
           }
           expect(Transformer.transform(message)).to eq({
-            event: "_#{Defaults::Redshift::RESERVED_WORDS[2]}",
-          }.merge(Defaults::Redshift::RESERVED_WORDS.to_h { |k| ["_#{k.downcase}", k] }))
+            event: "_all",
+            event_text: "ALL",
+          }.merge(Defaults::Redshift::RESERVED_WORDS.transform_keys { |k| "_#{k.downcase}" }).stringify_keys)
         end
       end
 
       describe '#prefix_reserved_words' do
         it 'prefixes all redshift reserved words' do
           message = {
-            event: Defaults::Redshift::RESERVED_WORDS[2],
-          }.merge(Defaults::Redshift::RESERVED_WORDS.to_h { |k| [k, k] })
+            "event" => "ALL",
+          }.merge(Defaults::Redshift::RESERVED_WORDS.to_h { |k| [k, true] })
 
           expect(Transformer.prefix_reserved_words(message)).to eq({
-            event: "_#{Defaults::Redshift::RESERVED_WORDS[2]}",
-          }.merge(Defaults::Redshift::RESERVED_WORDS.to_h { |k| ["_#{k}", k] }))
+            "event" => "_ALL"
+          }.merge(Defaults::Redshift::RESERVED_WORDS.transform_keys { |k| "_#{k}" }))
         end
       end
 
